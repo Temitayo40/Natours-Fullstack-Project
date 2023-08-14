2e06adf09b90df2232e927815b3f23777ebb943c
Adebowale <adebowaletemitayo40@gmail.com>
2023-08-14 12:45:45 +0100

    Server Side Rendering with PUG Template

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,7 +14,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLEWARES
+//Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 //set security http headers
 app.use(helmet());
 
@@ -55,7 +62,7 @@ app.use(
 );
 
 //Serving static files
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
 
 //Test middleware
 app.use((req, res, next) => {
@@ -64,6 +71,9 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
