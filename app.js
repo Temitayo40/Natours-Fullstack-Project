d6432133cd59a57cddffe316dd6446d473e8b570
Adebowale <adebowaletemitayo40@gmail.com>
2023-08-15 15:49:07 +0100

    Added the Login Functionality of the page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 const AppError = require('./utils/appError');
 const globalErrorController = require('./controllers/errorController');
 
@@ -42,6 +43,7 @@ app.use('/api', limiter);
 
 //Body parser, reading data frok the body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 //Data sanitization against NOSQL  query INJECTION
 app.use(mongoSanitize());
@@ -69,6 +71,7 @@ app.use(
 //Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
